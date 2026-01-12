eb424cec5ae97adaa68433a56b8705a395330749
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-12 17:14:18 +0500

    feat: add google and facebook login assets

--- lib/utils/constants/images.dart
+++ lib/utils/constants/images.dart
@@ -5,4 +5,9 @@
   static const String onboarding1Animation = 'assets/animations/welcome.json';
   static const String onboarding2Animation = 'assets/animations/cart.json';
   static const String onboarding3Animations= 'assets/animations/delivery.json';
+
+
+  static const String facebookIcon = 'assets/logo/facebook-icon.png';
+  static const String googleIcon= 'assets/logo/google-icon.png';
+
  }
