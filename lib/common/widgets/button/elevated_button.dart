79e5b23e3569d3b4a27fdf42cad04c83c7b9a92c
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-12 17:14:32 +0500

    style: improve login UI and update dependencies

--- lib/common/widgets/button/elevated_button.dart
+++ lib/common/widgets/button/elevated_button.dart
@@ -1,4 +1,4 @@
-import 'package:e_commerce/utils/constants/sizes.dart';
+
 import 'package:e_commerce/utils/helpers/device_helpers.dart';
 import 'package:flutter/material.dart';
 
--- lib/utils/constants/texts.dart
+++ lib/utils/constants/texts.dart
@@ -10,4 +10,17 @@ class UText{
   static const String onBoardingSubtitle1 = 'Your one-stop destination for effortless and enjoyable shopping';
   static const String onBoardingSubtitle2 = 'Discover top-quality products at the best prices with a seamless shopping experience';
   static const String onBoardingSubtitle3 = 'Get your favorite items delivered to your doorstep, anytime, anywhere';
+
+  //...login screen ...//
+
+  static const String loginTitle = 'Shop Smarter';
+  static const String loginSubTitle = 'Log in to Access Exclusive Deals and Simplify Your Shopping Experience';
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String rememberMe = 'Remember Re';
+  static const String forgetPassword = 'Forget Password';
+  static const String signIn = 'Sign In';
+  static const String createAccount = 'Create Account';
+  static const String orSignWith = 'Or Sign In With';
+
 }
