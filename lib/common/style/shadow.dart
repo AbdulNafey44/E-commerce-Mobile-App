01023662ca30c788ea6dcb38c393cafbf5b099ba
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-16 15:22:22 +0500

    using shadow as a separate reuseable widget

---
+++ lib/common/style/shadow.dart
@@ -0,0 +1,18 @@
+
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class UShadow{
+
+  UShadow._();
+
+  static List<BoxShadow> searchBarShadow = [
+
+      BoxShadow(
+            color: UColors.black.withValues(alpha: 0.1),
+            spreadRadius: 2.0,
+            blurRadius: 4.0,
+          ),
+
+];
+}
