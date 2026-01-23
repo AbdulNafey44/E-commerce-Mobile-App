95d9965872bf2c2ccbc1b3c21f407e39015dc26a
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-23 15:31:31 +0500

    adding a new reuseable promo_code widget

---
+++ lib/common/widgets/textfields/promo_code.dart
@@ -0,0 +1,53 @@
+import 'package:e_commerce/utils/helpers/helper_function.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/sizes.dart';
+import '../customeshape/rounded_container.dart';
+
+class UPromoCodeField extends StatelessWidget {
+  const UPromoCodeField({
+    super.key,
+
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final bool dark = UHelperFunction.isDarkMode(context);
+    return URoundedContainer(
+      backgroundColor: Colors.transparent,
+      padding: EdgeInsets.only(
+        left: USizes.md,
+        top: USizes.sm,
+        bottom: USizes.sm,
+        right: USizes.sm,
+      ),
+      showBorder: true,
+      child: Row(
+        children: [
+          Flexible(
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: 'Have a promo code?, enter here',
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none
+              ),
+            ),
+          ),
+          SizedBox( width: 80.0,
+              child: ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey.withValues(alpha: 0.2),
+                    foregroundColor: dark ? UColors.white.withValues(alpha: 0.5) : UColors.dark.withValues(alpha: 0.5),
+                    side: BorderSide(color: Colors.grey.withValues(alpha: 0.1)),
+                  ), child: Text('Apply'))),
+        ],
+      ),
+    );
+  }
+}
