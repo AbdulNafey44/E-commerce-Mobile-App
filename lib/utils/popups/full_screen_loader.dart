03129e13e29b523213023ec1d516c581dc9e8443
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-24 21:59:51 +0500

    adding full_screen_loader widget

---
+++ lib/utils/popups/full_screen_loader.dart
@@ -0,0 +1,33 @@
+import 'package:e_commerce/utils/popups/widgets/animation_loader.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../constants/colors.dart';
+import '../helpers/helper_function.dart';
+
+class UFullScreenLoader {
+  static void openLoadingDialog(String text) {
+    showDialog(
+        context: Get.overlayContext!,
+        barrierDismissible: false,
+        builder: (_) => PopScope(
+            canPop: false,
+            child: Container(
+              color: UHelperFunction.isDarkMode(Get.context!) ? UColors.dark : UColors.white,
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                children: [
+                  /// Extra Space
+                  const SizedBox(height: 250),
+
+                  /// Animation
+                  UAnimationLoader(text: text)
+                ],
+              ),
+            )));
+  }
+
+  static stopLoading() {
+    Navigator.of(Get.overlayContext!).pop();
+  }
+}
