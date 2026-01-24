702bf8849c54fec454282757aed79a69e270fa06
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-24 21:59:08 +0500

    adding animation_loader widget that show animation while loading data

---
+++ lib/utils/popups/widgets/animation_loader.dart
@@ -0,0 +1,31 @@
+import 'package:e_commerce/utils/constants/images.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../constants/sizes.dart';
+
+
+class UAnimationLoader extends StatelessWidget {
+  const UAnimationLoader({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          /// Animation
+          Lottie.asset(UImages.loadingAnimation, width: Get.width * 0.8),
+          const SizedBox(height: USizes.defaultSpace),
+
+          /// Title
+          Text(text, style: Theme.of(context).textTheme.bodyMedium, textAlign: TextAlign.center),
+          const SizedBox(height: USizes.defaultSpace),
+        ],
+      ),
+    );
+  }
+}
