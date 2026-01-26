43fd900058ab0433300124833a8b6c3e1829fedd
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-01-26 22:51:02 +0500

    Adding a user_controller that authenticate user through Google with firebase

---
+++ lib/features/personalization/controller/user_controller.dart
@@ -0,0 +1,38 @@
+import 'package:e_commerce/data/repositories/user/user_repository.dart';
+import 'package:e_commerce/features/authentication/models/user_model.dart';
+import 'package:e_commerce/utils/popups/snackbar_helpers.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class userController extends GetxController{
+
+  static userController get instance => Get.find();
+
+  /// Variables
+ final _userRepository = Get.put(UserRepository());
+
+
+ /// Function to save user record
+   Future<void> saveUserRecord(UserCredential userCredential)  async {
+
+     try{
+       // convert first name & lastname
+       final nameParts = UserModel.nameParts(userCredential.user!.displayName);
+       final username = '${userCredential.user!.displayName}23422';
+      // create user model
+       UserModel userModel = UserModel(
+           id: userCredential.user!.uid,
+           firstName: nameParts[0],
+           lastName: nameParts.length > 1 ? nameParts.sublist(1).join(' ') : ' ' ,
+           username: username,
+           email: userCredential.user!.email ?? '',
+           phoneNumber: userCredential.user!.phoneNumber ?? '',
+           profilePicture: userCredential.user!.photoURL ?? '');
+       // save user record
+      await _userRepository.saveUserRecord(userModel);
+
+     }catch(e){
+       USnackBarHelpers.warningSnackBar(title: 'Data not save', message: 'Something went wrong while saving your data');
+     }
+   }
+}
