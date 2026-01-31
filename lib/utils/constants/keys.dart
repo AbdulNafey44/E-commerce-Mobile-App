c7d144ab04a1a766385ae59df4850d3485faef6f
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-02-01 00:11:47 +0500

    update keys adding cloudinary Apis

--- lib/utils/constants/keys.dart
+++ lib/utils/constants/keys.dart
@@ -5,4 +5,11 @@ class UKeys {
   static const String rememberMeEmail = 'Remember_email';
   static const String rememberMePassword = 'Remember_password';
 
+
+  static const String cloudName = 'dixbfe7cl';
+  static const String uploadPreset = 'upload_image';
+  static const String profileFolder = 'Profile Pictures';
+  static const String apiKey = '753979322891439';
+  static const String apiSecret = '2cObSJsOwN4bN9rxWsnyqAAyLp4';
+
 }
