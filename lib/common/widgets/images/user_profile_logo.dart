e39084ae1d28abea50fd3dcae144a7616a16b1ab
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-02-01 00:10:38 +0500

    update user_profile_logo file

--- lib/common/widgets/images/user_profile_logo.dart
+++ lib/common/widgets/images/user_profile_logo.dart
@@ -1,7 +1,9 @@
 import 'package:e_commerce/common/widgets/images/circular_image.dart';
+import 'package:e_commerce/common/widgets/shimmer/shimmer_effect.dart';
 import 'package:e_commerce/features/personalization/controller/user_controller.dart';
 import 'package:e_commerce/utils/constants/images.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 class UserProfileLogo extends StatelessWidget {
@@ -12,13 +14,21 @@ class UserProfileLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = userController.instance;
-    bool isProfileAvailable = controller.user.value.profilePicture.isNotEmpty;
-    return UCircularImage(image: isProfileAvailable ?controller.user.value.profilePicture : UImages.profileLogo,
-      isNetworkImage: isProfileAvailable ? true : false,
-      height: 120.0,
-      width: 120.0,
-      borderWidth: 5.0,
-      padding: 0,
-    );
+
+    return
+      Obx(
+        () {
+          bool isProfileAvailable = controller.user.value.profilePicture.isNotEmpty;
+          if(controller.isProfileUploading.value){
+            return UShimmerEffect(width: 120, height: 120, radius: 120);
+          }
+          return  UCircularImage(image: isProfileAvailable ?controller.user.value.profilePicture : UImages.profileLogo,
+            isNetworkImage: isProfileAvailable ? true : false,
+            height: 120.0,
+            width: 120.0,
+            borderWidth: 5.0,
+            padding: 0,);
+        }
+      );
   }
 }
