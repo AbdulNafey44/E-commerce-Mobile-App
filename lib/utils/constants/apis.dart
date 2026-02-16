16870b0496fe48edd5d13604b7b89948e4145ddf
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-02-16 16:13:18 +0500

    Remove API keys file from Git tracking

--- lib/utils/constants/apis.dart
+++
@@ -1,7 +0,0 @@
-
-class UapiUrls{
-
-  static String uploadApi(String cloudName) => 'https://api.cloudinary.com/v1_1/$cloudName/image/upload';
-
-  static String deleteApi(String cloudName) => 'https://api.cloudinary.com/v1_1/$cloudName/image/destroy';
-}
