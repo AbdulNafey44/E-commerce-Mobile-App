c38bb8f05edc9dac80fbf2700888002cf06c2ea8
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-02-16 15:44:19 +0500

    modified code

--- lib/features/personalization/screens/edit_profile_screen/widgets/user_profile_with_edit_icon.dart
+++ lib/features/personalization/screens/edit_profile_screen/widgets/user_profile_with_edit_icon.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce/features/personalization/controller/user_controller.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../../common/widgets/icons/circular_icon.dart';
@@ -12,15 +15,24 @@ class UserProfileWithEditIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = userController.instance;
     return Stack(
       children: [
         Center(child: UserProfileLogo()),
-        Positioned(
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Center(child: UCircularIcon(icon: Iconsax.edit))),
+        Obx(
+                () {
+              if (controller.isProfileUploading.value) {
+                return SizedBox();
+              }
+              return Positioned(
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Center(child: UCircularIcon(icon: Iconsax.edit,
+                    onPressed: controller.updateUserProfilePicture,)));
+            }
+        ),
       ],
     );
   }
