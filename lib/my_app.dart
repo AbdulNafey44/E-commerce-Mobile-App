167444d8f93520e31c6146adb7bfcc005d06ef14
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-02-21 00:57:24 +0500

    update code

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/bindings/bindings.dart';
+import 'package:e_commerce/routs/app_routes.dart';
 
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/theme/theme.dart';
@@ -17,6 +18,8 @@ class MyApp extends StatelessWidget {
        themeMode: ThemeMode.system,
       theme: UAppTheme.lightTheme,
       darkTheme: UAppTheme.darkTheme,
+
+      getPages: UAppRoutes.screens,
         home: Scaffold(
           backgroundColor: UColors.primary,
           body: Center(
