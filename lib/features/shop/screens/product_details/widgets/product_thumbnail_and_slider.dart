02b0fdf842ec2359a4cf3f4748a132c80a5f9b6d
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-04 02:06:14 +0500

    update code

--- lib/features/shop/screens/product_details/widgets/product_thumbnail_and_slider.dart
+++ lib/features/shop/screens/product_details/widgets/product_thumbnail_and_slider.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce/features/shop/controller/product/image_controller.dart';
+import 'package:e_commerce/features/shop/models/product_model.dart';
 import 'package:e_commerce/utils/helpers/helper_function.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../../common/widgets/appbar/appbar.dart';
@@ -9,30 +13,46 @@ import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/images.dart';
 import '../../../../../utils/constants/sizes.dart';
 
-
-
 class UProductThumbnailAndSlider extends StatelessWidget {
-  const UProductThumbnailAndSlider({
-    super.key,
-  });
+  const UProductThumbnailAndSlider({super.key, required this.product});
+
+  final ProductModel product;
 
   @override
   Widget build(BuildContext context) {
     final bool dark = UHelperFunction.isDarkMode(context);
+    final controller = Get.put(ImageController());
+    List<String> images = controller.getAllProductImages(product);
     return Container(
-      color: dark? UColors.dark: UColors.light,
+      color: dark ? UColors.dark : UColors.light,
       child: Stack(
         children: [
           ///image thumbnail
           SizedBox(
             height: 400,
             child: Padding(
-              padding: const EdgeInsets.all(
-                USizes.productImageRadius * 2,
+              padding: const EdgeInsets.all(USizes.productImageRadius * 2),
+              child: Center(
+                child: Obx(
+                      (){
+                        final image = controller.selectedProductImage.value;
+                        return GestureDetector(
+                          onTap: (){},
+                          child: CachedNetworkImage(
+                            imageUrl: image,
+                            progressIndicatorBuilder: (context, url, progress) =>
+                                CircularProgressIndicator(
+                                  color: UColors.primary,
+                                  value: progress.progress,
+                                ),
+                          ),
+                        );
+                      }
+                ),
               ),
-              child: Center(child: Image(image: AssetImage(UImages.productImage28))),
             ),
           ),
+
           /// Image Slider
           Positioned(
             left: USizes.defaultSpace,
@@ -50,18 +70,18 @@ class UProductThumbnailAndSlider extends StatelessWidget {
                   border: Border.all(color: UColors.primary),
                   imageUrl: (UImages.productImage27),
                 ),
-                separatorBuilder: (context, index) => SizedBox(width: USizes.spaceBtwItems,),
+                separatorBuilder: (context, index) =>
+                    SizedBox(width: USizes.spaceBtwItems),
                 itemCount: 6,
               ),
             ),
           ),
+
           /// back arrow and fav icon ///
           UAppBar(
             showBackArrow: true,
-            actions: [
-              UCircularIcon(icon: Iconsax.heart5, color: Colors.red,),
-            ],
-          )
+            actions: [UCircularIcon(icon: Iconsax.heart5, color: Colors.red)],
+          ),
         ],
       ),
     );
