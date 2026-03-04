146cd5e1ab066c848b531df51832fc729012b541
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-05 01:58:40 +0500

    update: enhance product meta data display

--- lib/features/shop/screens/product_details/widgets/product_meta_data.dart
+++ lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -1,3 +1,7 @@
+import 'package:e_commerce/features/shop/controller/product/product_controller.dart';
+import 'package:e_commerce/features/shop/models/product_model.dart';
+import 'package:e_commerce/utils/constants/enum.dart';
+import 'package:e_commerce/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
 
 
@@ -13,11 +17,14 @@ import '../../../../../utils/constants/sizes.dart';
 
 class UProductMetaData extends StatelessWidget {
   const UProductMetaData({
-    super.key,
+    super.key, required this.product,
   });
 
+  final ProductModel product ;
   @override
   Widget build(BuildContext context) {
+    final controller = ProductController.instance;
+    String? salePercentage = controller.calculateSalePercentage(product.price, product.salePrice);
     return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -25,32 +32,38 @@ class UProductMetaData extends StatelessWidget {
           Row(
             children: [
               /// Sale Tag
-              URoundedContainer(
-                radius: USizes.sm,
-                backgroundColor: UColors.yellow.withValues(alpha: 0.8),
-                padding: const EdgeInsets.symmetric(horizontal: USizes.sm, vertical: USizes.xs),
-                child: Text('20%', style: Theme.of(context).textTheme.labelLarge!.apply(color: UColors.black)),
-              ),
-              SizedBox(width: USizes.spaceBtwItems,),
+              if(salePercentage != null)...[
+                URoundedContainer(
+                  radius: USizes.sm,
+                  backgroundColor: UColors.yellow.withValues(alpha: 0.8),
+                  padding: const EdgeInsets.symmetric(horizontal: USizes.sm, vertical: USizes.xs),
+                  child: Text('$salePercentage%', style: Theme.of(context).textTheme.labelLarge!.apply(color: UColors.black)),
+                ),
+                SizedBox(width: USizes.spaceBtwItems,),
+              ],
+
+              /// Actual Price
+              if(product.productType == ProductType.single.toString() && product.salePrice > 0)...[
+                Text('${UText.currency}${product.price.toStringAsFixed(0)}',style: Theme.of(context).textTheme.titleSmall!.apply(decoration: TextDecoration.lineThrough),),
+                SizedBox(width: USizes.spaceBtwItems),
+              ],
+
               /// sale price
-              Text('\$250',style: Theme.of(context).textTheme.titleSmall!.apply(decoration: TextDecoration.lineThrough),),
-              SizedBox(width: USizes.spaceBtwItems,),
-              /// actual price
-              UProductPriceText(price: '120', isLarge: true,),
+              UProductPriceText(price: controller.getProductPrice(product), isLarge: true,),
               Spacer(),
               IconButton(onPressed: (){}, icon: Icon(Icons.share)),
             ],
           ),
           SizedBox(height: USizes.spaceBtwItems / 1.5,),
           /// product title
-          UProductTitleText(title: 'Face Wash'),
+          UProductTitleText(title: product.title),
           SizedBox(height: USizes.spaceBtwItems / 1.5,),
           /// product status
           Row(
             children: [
               UProductTitleText(title: 'Status'),
               SizedBox(width: USizes.spaceBtwItems),
-              Text('In Stock',style: Theme.of(context).textTheme.titleMedium,)
+              Text(controller.getProductStatus(product.stock),style: Theme.of(context).textTheme.titleMedium,)
             ],
           ),
           SizedBox(height: USizes.spaceBtwItems,),
@@ -60,9 +73,10 @@ class UProductMetaData extends StatelessWidget {
               ///  product brand image with title
               UCircularImage(
                 padding: 0,
-                image: UImages.dariMoochLogo,width: 34.0,height: 34.0,),
+                isNetworkImage: true,
+                image: product.brand != null ? product.brand!.image : '',width: 34.0,height: 34.0,),
               SizedBox(width: USizes.spaceBtwItems,),
-              UBrandTitleWithverifyIcon(title: 'Darhi Mooch'),
+              UBrandTitleWithverifyIcon(title: product.brand != null ? product.brand!.name : ''),
             ],
           )
 
