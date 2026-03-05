2b1d0cf8898d0ce7bd51957f443572564637a67d
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-06 00:34:44 +0500

    Update all products screen

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,27 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce/common/style/padding.dart';
 import 'package:e_commerce/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce/common/widgets/shimmer/vertical_product_shimmer.dart';
+import 'package:e_commerce/features/shop/controller/product/all_products_controller.dart';
+import 'package:e_commerce/utils/helpers/cloud_helper_functions.dart';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 
 import '../../../../common/widgets/products/sortable_products.dart';
+import '../../models/product_model.dart';
 
 class AllProductsScreen extends StatelessWidget {
-  const AllProductsScreen({super.key});
+  const AllProductsScreen({super.key, this.query, this.futureMethod, required this.title});
+
+  final String title;
+  final Future<List<ProductModel>>? futureMethod ;
+  final Query? query;
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(AllProductsController());
     return Scaffold(
       appBar: UAppBar(
         showBackArrow: true,
         title: Text(
-          'Popular Products',
+          title,
           style: Theme.of(context).textTheme.headlineMedium,
         ),
       ),
       body: SingleChildScrollView(
         child: Padding(padding: UPading.screenPading,
-          child: UScrolableProducts(),
+          child: FutureBuilder(
+            future: futureMethod ?? controller.fetchProductsByQuery(query),
+            builder: (context, Snapshot) {
+              const loader = UVerticalProductShimmer();
+            final widget =  UCloudHelperFunctions.checkMultiRecordState(snapshot: Snapshot, loader: loader);
+            if(widget != null)return widget ;
+               List<ProductModel> products = Snapshot.data!;
+              return UScrolableProducts(products: products);
+            }
+          ),
 
         ),
 
