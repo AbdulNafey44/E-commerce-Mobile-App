44423f37b885da0279d77eb36975a538985f21c8
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-07 03:49:14 +0500

    update brand showcase widget

--- lib/common/widgets/brands/brand_showcase.dart
+++ lib/common/widgets/brands/brand_showcase.dart
@@ -1,5 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce/common/widgets/shimmer/shimmer_effect.dart';
+import 'package:e_commerce/features/shop/screens/brands/brand_products.dart';
 import 'package:e_commerce/utils/helpers/helper_function.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import '../../../features/shop/models/brand_model.dart';
 import '../../../utils/constants/colors.dart';
@@ -7,30 +12,34 @@ import '../../../utils/constants/sizes.dart';
 import '../customeshape/rounded_container.dart';
 import 'brand_card.dart';
 
-
 class UBrandShowCase extends StatelessWidget {
-  const UBrandShowCase({
-    super.key, required this.images, required this.brand,
-  });
+  const UBrandShowCase({super.key, required this.images, required this.brand});
+
   final List<String> images;
   final BrandModel brand;
+
   @override
   Widget build(BuildContext context) {
     final bool dark = UHelperFunction.isDarkMode(context);
-    return URoundedContainer(
-      showBorder: true,
-      borderColor: UColors.darkerGrey,
-      backgroundColor: Colors.transparent,
-      padding: EdgeInsets.all(USizes.md),
-      margin: EdgeInsets.only(bottom: USizes.spaceBtwItems),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          UBrandCard(showBorder: false, brand: brand),
-         Row(
-             children:  images.map((image) => BuildBrandImage(dark, image)).toList()
-         )
-      ],
+    return InkWell(
+      onTap: () => Get.to(() => BrandsProductsScreen(title: brand.name, brand: brand)),
+      child: URoundedContainer(
+        showBorder: true,
+        borderColor: UColors.darkerGrey,
+        backgroundColor: Colors.transparent,
+        padding: EdgeInsets.all(USizes.md),
+        margin: EdgeInsets.only(bottom: USizes.spaceBtwItems),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            UBrandCard(showBorder: false, brand: brand),
+            Row(
+              children: images
+                  .map((image) => BuildBrandImage(dark, image))
+                  .toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -38,12 +47,18 @@ class UBrandShowCase extends StatelessWidget {
   Widget BuildBrandImage(bool dark, String image) {
     return Expanded(
       child: URoundedContainer(
-            height: 100,
-            margin: const EdgeInsets.only(right: USizes.sm),
-            padding: const EdgeInsets.all(USizes.md),
-            backgroundColor: dark ? UColors.darkerGrey : UColors.light,
-            child: Image(image: AssetImage(image)),
-          ),
+        height: 100,
+        margin: const EdgeInsets.only(right: USizes.sm),
+        padding: const EdgeInsets.all(USizes.md),
+        backgroundColor: dark ? UColors.darkerGrey : UColors.light,
+        child: CachedNetworkImage(
+          imageUrl: image,
+          fit: BoxFit.contain,
+          progressIndicatorBuilder: (context, url, progress) =>
+              UShimmerEffect(width: 100, height: 100),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
+      ),
     );
   }
 }
