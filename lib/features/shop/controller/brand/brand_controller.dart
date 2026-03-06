2b1f81e95fc91fe84629a193fc86cc642d59fa6d
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-07 03:49:55 +0500

    update brand controller

--- lib/features/shop/controller/brand/brand_controller.dart
+++ lib/features/shop/controller/brand/brand_controller.dart
@@ -1,6 +1,8 @@
 
 import 'package:e_commerce/data/repositories/brand/brand_repository.dart';
+import 'package:e_commerce/data/repositories/product/product_repository.dart';
 import 'package:e_commerce/features/shop/models/brand_model.dart';
+import 'package:e_commerce/features/shop/models/product_model.dart';
 import 'package:e_commerce/utils/popups/snackbar_helpers.dart';
 import 'package:get/get.dart';
 
@@ -21,7 +23,7 @@ class BrandController extends GetxController{
     getBrands();
     super.onInit();
   }
-
+ /// function to fetch brands
   Future<void> getBrands() async {
     try{
 
@@ -40,5 +42,26 @@ class BrandController extends GetxController{
       isLoading.value = false ;
     }
   }
+ /// function to fetch specific brands
+  Future<List<ProductModel>> getBrandProducts(String brandId, {int limit = -1}) async {
+    try{
+    List<ProductModel> products = await ProductRepository.instance.getProductsForBrand(brandId: brandId, limit: limit);
+    return products;
+    }catch(e){
+      USnackBarHelpers.errorSnackBar(title: 'Failed', message: e.toString());
+      return [];
+    }
+  }
+
+  /// get brands for specific products
+   Future<List<BrandModel>> getBrandsForCategory(String categoryId) async {
+    try{
+      final brands = await _repository.fetchBrandsForCategory(categoryId);
+      return brands ;
+    }catch(e){
+      USnackBarHelpers.errorSnackBar(title: 'Failed', message: e.toString());
+      return [] ;
+    }
+   }
 
 }
