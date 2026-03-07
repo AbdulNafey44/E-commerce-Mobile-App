4cf76ff46baeb93548309cf2fa88c70ee9bfd04c
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-08 02:11:13 +0500

    Update wishlist.dart: add/remove functionality improvements

--- lib/features/shop/screens/wishlist/wishlist.dart
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -2,13 +2,19 @@ import 'package:e_commerce/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce/common/widgets/icons/circular_icon.dart';
 import 'package:e_commerce/common/widgets/layouts/grid_layout.dart';
 import 'package:e_commerce/common/widgets/products/product_card/product_card_vertical.dart';
+import 'package:e_commerce/common/widgets/shimmer/vertical_product_shimmer.dart';
+import 'package:e_commerce/features/shop/controller/product/favourite_controller.dart';
 
 import 'package:e_commerce/navigation_menu.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/helpers/cloud_helper_functions.dart';
+import 'package:e_commerce/utils/popups/widgets/animation_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'package:iconsax/iconsax.dart';
 
+import '../../../../utils/constants/images.dart';
 import '../../models/product_model.dart';
 
 
@@ -33,8 +39,25 @@ class WishlistScreen extends StatelessWidget {
         ),
         body: SingleChildScrollView(child: Padding(
             padding: const EdgeInsets.all(USizes.defaultSpace),
-            child: UGridLayout(itemCount: 10,
-                itemBuilder: (context, index) => UProductCard(product: ProductModel.empty(),))),
+            child: Obx(
+                () => FutureBuilder(
+                  future: FavouriteController.instance.getFavouriteProducts(),
+                  builder: (context, snapshot) {
+                    final nothingFound = UAnimationLoader
+                      (text: 'Wishlist is empty...',
+                       animation: UImages.pencilAnimation,
+                       showActionButton: true,
+                       actionText: "let`s add some",
+                       onActionPressed: () => NavigationController.instance.selectedIndex.value= 0,
+                    );
+                    const loader = UVerticalProductShimmer(itemCount: 6);
+                    final widget = UCloudHelperFunctions.checkMultiRecordState(snapshot: snapshot, loader: loader , nothingFound: nothingFound);
+                    if(widget != null) return widget ;
+                    List<ProductModel> products = snapshot.data!;
+                    return UGridLayout(itemCount: products.length,
+                        itemBuilder: (context, index) => UProductCard(product: products[index]));
+                  }, ),
+            )),
         )
         );
     }
