e242f3a0f7f89172eae05e5d25e5785b9285f2f9
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-08 02:11:14 +0500

    Update animation_loader.dart: improve loading animation

--- lib/utils/popups/widgets/animation_loader.dart
+++ lib/utils/popups/widgets/animation_loader.dart
@@ -1,15 +1,27 @@
 
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
+import '../../constants/images.dart';
 import '../../constants/sizes.dart';
 
 
 class UAnimationLoader extends StatelessWidget {
-  const UAnimationLoader({super.key, required this.text});
+  const UAnimationLoader({super.key,
+    required this.text,
+    this.animation = UImages.loadingAnimation ,
+    this.showActionButton = false,
+    this.actionText,
+    this.onActionPressed
+  });
 
   final String text;
+  final String animation ;
+  final bool showActionButton;
+  final String? actionText;
+  final VoidCallback? onActionPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -18,12 +30,25 @@ class UAnimationLoader extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           /// Animation
-          Lottie.asset('assets/animations/loading.json', width: Get.width * 0.8),
+          Lottie.asset(animation, width: Get.width * 0.8),
           const SizedBox(height: USizes.defaultSpace),
 
           /// Title
           Text(text, style: Theme.of(context).textTheme.bodyMedium, textAlign: TextAlign.center),
           const SizedBox(height: USizes.defaultSpace),
+
+          showActionButton ?
+              SizedBox(
+                width: 250,
+                child: OutlinedButton(
+                    onPressed: onActionPressed,
+                    style: OutlinedButton.styleFrom(backgroundColor: UColors.dark),
+                    child: Text(
+                      actionText!,
+                      style: Theme.of(context).textTheme.bodyMedium!.apply(color: UColors.light),
+                    ),
+              )
+              ) : SizedBox()
         ],
       ),
     );
