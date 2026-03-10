ddd5d292c46e97922eb138b7ffe37b2ca0ae132b
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-10 14:43:11 +0500

    update code

--- lib/features/shop/screens/sub_categories/sub_category.dart
+++ lib/features/shop/screens/sub_categories/sub_category.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/common/style/padding.dart';
+import 'package:e_commerce/common/widgets/shimmer/horizontal_product_shimmer.dart';
 
 import 'package:e_commerce/common/widgets/text/section_heading.dart';
 import 'package:e_commerce/features/shop/controller/category/category_controller.dart';
@@ -13,76 +14,109 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
-
 import '../../../../common/widgets/appbar/appbar.dart';
-
-
+import '../all_products/all_products.dart';
 
 class SubCategoryScreen extends StatelessWidget {
   const SubCategoryScreen({super.key, required this.category});
 
-  final CategoryModel category ;
+  final CategoryModel category;
+
   @override
   Widget build(BuildContext context) {
     final bool dark = UHelperFunction.isDarkMode(context);
     final controller = Get.put(CategoryController());
     return Scaffold(
-      appBar: UAppBar( showBackArrow: true, title: Text(category.name, style: Theme.of(context).textTheme.headlineMedium)),
+      appBar: UAppBar(
+        showBackArrow: true,
+        title: Text(
+          category.name,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
       body: SingleChildScrollView(
-        child: Padding(padding: UPading.screenPading,
+        child: Padding(
+          padding: UPading.screenPading,
           child: Column(
             children: [
-
               /// fetch subcategory
               FutureBuilder(
-                  future: controller.getSubCategories(category.id),
-                  builder: (context, snapshot) {
-                    /// handle loading , error state
-                    final widget = UCloudHelperFunctions.checkMultiRecordState(snapshot: snapshot);
-                    if(widget != null) return widget;
-                    List<CategoryModel> subCategories = snapshot.data!;
-                    /// fetch product for sub category
-                return ListView.builder(
+                future: controller.getSubCategories(category.id),
+                builder: (context, snapshot) {
+                  /// handle loader , error , empty state
+                  const loader = UHorizontalProductShimmer();
+                  final widget = UCloudHelperFunctions.checkMultiRecordState(
+                    snapshot: snapshot, loader: loader
+                  );
+                  if (widget != null) return widget;
+                  List<CategoryModel> subCategories = snapshot.data!;
+
+                  /// fetch product for sub category
+                  return ListView.builder(
                     shrinkWrap: true,
-                      itemCount: subCategories.length,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        CategoryModel subCategory = subCategories[index];
-                          return FutureBuilder(
-                              future: controller.getCategoryProducts(categoryId: subCategory.id),
-                              builder: (context, snapshot) {
-                                /// handle error , empty state
-                                final widget = UCloudHelperFunctions.checkMultiRecordState(snapshot: snapshot);
-                                if(widget != null) return widget;
-                                List<ProductModel> products = snapshot.data!;
-                                  return Column(
-                                  children: [
-                                    /// sub category
-                                    USectionHeading(title: subCategory.name, onPressed: (){},),
-                                    SizedBox(height: USizes.spaceBtwItems/2),
+                    itemCount: subCategories.length,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
 
-                                    /// Horizontal Product Card
-                                    SizedBox(height: 120, child: ListView.separated(
-                                      separatorBuilder: (context, index) => SizedBox(width: USizes.spaceBtwItems/2),
-                                      itemCount: products.length,
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: (context, index) {
-                                        ProductModel product = products[index];
-                                        return UProductCardHorizontal(product: product);
-                                      },)),
-                                  ],
-                                );
-                              },);
-                      }, );
-                  }, ),
+                      CategoryModel subCategory = subCategories[index];
+                      return FutureBuilder(
+                        future: controller.getCategoryProducts(
+                          categoryId: subCategory.id,
+                        ),
+                        builder: (context, snapshot) {
+                          /// handle error , empty state
+                          final widget =
+                              UCloudHelperFunctions.checkMultiRecordState(
+                                snapshot: snapshot, loader: loader
+                              );
+                          if (widget != null) return widget;
+                          List<ProductModel> products = snapshot.data!;
+                          return Column(
+                            children: [
+                              /// sub category
+                              USectionHeading(
+                                title: subCategory.name,
+                                onPressed: () => Get.to(
+                                  () => AllProductsScreen(
+                                    title: subCategory.name,
+                                    futureMethod: controller
+                                        .getCategoryProducts(
+                                          categoryId: subCategory.id,
+                                          limit: -1,
+                                        ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: USizes.spaceBtwItems / 2),
 
+                              /// Horizontal Product Card
+                              SizedBox(
+                                height: 120,
+                                child: ListView.separated(
+                                  separatorBuilder: (context, index) =>
+                                      SizedBox(width: USizes.spaceBtwItems / 2),
+                                  itemCount: products.length,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) {
+                                    ProductModel product = products[index];
+                                    return UProductCardHorizontal(
+                                      product: product,
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
             ],
           ),
-        
         ),
       ),
     );
   }
 }
-
-
