95005b71099b4fd5aaa00d22a3f690d5b18fecbb
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-13 02:08:11 +0500

    Implement cart items list widget

--- lib/features/shop/screens/cart/widgets/cart_items.dart
+++ lib/features/shop/screens/cart/widgets/cart_items.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce/features/shop/controller/cart/cart_controller.dart';
 import 'package:e_commerce/features/shop/screens/cart/widgets/product_quantity_with_add_remove.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../../common/widgets/products/cart/cart_item.dart';
 import '../../../../../common/widgets/text/product_price_text.dart';
@@ -13,28 +15,38 @@ class UCartItems extends StatelessWidget {
 final bool showAddRemoveButtons ;
   @override
   Widget build(BuildContext context) {
+    final controller = CartController.instance;
     return ListView.separated(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       separatorBuilder: (context, index) =>
           SizedBox(height: USizes.spaceBtwSections),
-      itemCount: 2,
+      itemCount: controller.cartItems.length,
       itemBuilder: (context, index) {
-        return Column(
-          children: [
-            /// cart itom
-            UCartItom(),
-            if(showAddRemoveButtons) SizedBox(height: USizes.spaceBtwItems),
-            if(showAddRemoveButtons) Row(
-              children: [
-                /// extra space
-                SizedBox(width: 70),
-                /// Quantity Buttons
-                UProductQuantityWithAddRemove(),
-                Spacer(),
-                UProductPriceText(price: '120'),
-              ],
-            ),
-          ],
+        return Obx(
+            () {
+              final cartItem = controller.cartItems[index];
+              return  Column(
+                children: [
+                  /// cart item
+                  UCartItom(cartItem: cartItem),
+                  if(showAddRemoveButtons) SizedBox(height: USizes.spaceBtwItems),
+                  if(showAddRemoveButtons) Row(
+                    children: [
+                      /// extra space
+                      SizedBox(width: 70),
+                      /// Quantity Buttons
+                      UProductQuantityWithAddRemove(
+                        quantity: cartItem.quantity,
+                         add: () => controller.addOneToCart(cartItem),
+                          remove: () => controller.removeOneFromCart(cartItem)),
+                      Spacer(),
+                      UProductPriceText(price: (cartItem.price * cartItem.quantity).toStringAsFixed(0)),
+                    ],
+                  ),
+                ],
+              );
+            }
         );
 
       },
