96f450a07dccd93e294cf64f1e1362cc7029df8b
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-13 02:08:14 +0500

    Add quantity increment and decrement controls for cart items

--- lib/features/shop/screens/cart/widgets/product_quantity_with_add_remove.dart
+++ lib/features/shop/screens/cart/widgets/product_quantity_with_add_remove.dart
@@ -9,19 +9,22 @@ import '../../../../../utils/constants/sizes.dart';
 
 class UProductQuantityWithAddRemove extends StatelessWidget {
   const UProductQuantityWithAddRemove({
-    super.key,
+    super.key, required this.quantity, this.add, this.remove,
 
   });
 
 
-
+  final int quantity;
+  final VoidCallback? add, remove;
   @override
   Widget build(BuildContext context) {
     final bool dark = UHelperFunction.isDarkMode(context);
+
     return Row(
       children: [
         /// decrement btn
         UCircularIcon(
+          onPressed: remove,
           icon: Iconsax.minus,
           width: 32,
           height: 32,
@@ -34,12 +37,13 @@ class UProductQuantityWithAddRemove extends StatelessWidget {
         SizedBox(width: USizes.spaceBtwItems),
         /// Text
         Text(
-          '2',
+          quantity.toString(),
           style: Theme.of(context).textTheme.titleSmall,
         ),
         SizedBox(width: USizes.spaceBtwItems),
         /// Increment btn
         UCircularIcon(
+          onPressed: add,
           icon: Iconsax.add,
           width: 32,
           height: 32,
