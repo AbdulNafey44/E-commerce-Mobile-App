cb04324b66dbefc32014752819070a850d9fe818
AbdulNafey44 <abdulnaffy555@gmail.com>
2026-03-13 02:08:10 +0500

    Create cart screen layout and integrate cart controller

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,13 +1,18 @@
 import 'package:e_commerce/common/style/padding.dart';
 import 'package:e_commerce/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce/common/widgets/button/elevated_button.dart';
+import 'package:e_commerce/common/widgets/icons/circular_icon.dart';
+import 'package:e_commerce/common/widgets/loaders/animation_loader.dart';
+import 'package:e_commerce/features/shop/controller/cart/cart_controller.dart';
 
 import 'package:e_commerce/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:e_commerce/utils/constants/images.dart';
 
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:iconsax/iconsax.dart';
 
 import '../checkout/checkout.dart';
 
@@ -16,26 +21,48 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = CartController.instance;
     return Scaffold(
       ///...[App Bar]...///
       appBar: UAppBar(
         showBackArrow: true,
         title: Text('Cart', style: Theme.of(context).textTheme.headlineMedium),
+        actions: [UCircularIcon(icon: Iconsax.box_remove, onPressed: controller.clearCart)],
       ),
 
       ///...[Body]...///
-      body: Padding(
-        padding: UPading.screenPading,
-        child: UCartItems(),
+      body: Obx(
+        () {
+          final emptyWidget = UAnimationLoader(
+              text: 'Cart is empty',
+              animation: UImages.cartEmptyAnimation,
+            showActionButton: true,
+            actionText: "lets fill it",
+            onActionPressed:  Get.back,
+          );
+          if(controller.cartItems.isEmpty){
+            return emptyWidget;
+          }
+          return SingleChildScrollView(
+            child: Padding(
+              padding: UPading.screenPading,
+              child: UCartItems(),
+            ),
+          );
+        }
       ),
       ///....[Bottom]...//
-      bottomNavigationBar: Padding(
-        padding:EdgeInsets.all(USizes.defaultSpace),
-        child: UElevatedButton(onPressed: ()=>  Get.to(CheckOutScreen()), child: Text('Checkout \$1200')),
+      bottomNavigationBar: Obx(
+          () {
+            if(controller.cartItems.isEmpty) return SizedBox();
+            return Padding(
+              padding:EdgeInsets.all(USizes.defaultSpace),
+              child: UElevatedButton(onPressed: ()=>  Get.to(CheckOutScreen()), child: Text('Checkout \$${controller.totalCartPrice.value.toStringAsFixed(2)}')),
+            );
+          }
       ),
     );
   }
 }
-
 
 
